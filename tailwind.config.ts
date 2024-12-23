82e30e89f6d8a38d3e8d2c3b4c59e3f3ca312951
steven <steven841221@gmail.com>
2024-12-23 23:41:01 +1100

    set up prettier configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,14 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xxs: "320px",
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xlg: "1280px",
+    },
     extend: {
       colors: {
         background: "var(--background)",
